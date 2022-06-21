3228b0ebe8c1a9bffc4a144e44ca46c5ed1ef771
AlejandroDalzotto <105230777+AlejandroDalzotto@users.noreply.github.com>
2022-06-20 21:03:37 -0300

    Últimos cambios - 20/6/2022
    No probé la base de datos, fíjate si el archivo SQL anda correctamente y dropea la base de datos de forma correcta, ya que le faltaba lo de DROP DATABASE y bueno lo de crear y usar también.
    Mas allá de eso esta todo, creo...

--- stocktracking.sql
+++ stocktracking.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-06-2022 a las 12:18:17
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
+-- Tiempo de generación: 21-06-2022 a las 01:58:20
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,8 +44,27 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `Nombre`, `Descripcion`, `Cantidad`, `Unidad`) VALUES
-(1, 'Jeringas', 'se usan para suministrar una cantidad', 10, 'mm'),
-(3, 'Jeringas', 'objeto de prueba para el front end', 10, 'kg');
+(1, 'ACEITE BOMBA VACIO x 5lts', '', 10, 'Litro'),
+(2, 'ADENITIS EQUINA x 10 Ds', '', 14, 'Botella'),
+(3, 'AGITA 10 WG x 400 grs', '', 2, 'Botella'),
+(4, 'AGUJAS HIPOD. ACERO', '', 348, 'Dispositivo'),
+(5, 'AGUJAS HIPODERMICAS Medidas Varias', '', 6, 'Dispositivo'),
+(6, 'AQ30 X 22Kg (ACIDO PERACETICO)', '', 44, 'Mililitros'),
+(7, 'ARMOR Sellador Barrera (1 Lts)', '', 1200, 'Litro'),
+(8, 'ARRASA x 5 lts', '', 10, 'Dispositivo'),
+(9, 'BAGO ADE3E Fsco X 250 CC', '', 4, 'Botella'),
+(10, 'Boyero 220-12 volt. De 35Km', '', 1, 'Dispositivo'),
+(11, 'BUSCAPINA COMPUESTA - Frasco x 50ml', '', 4, 'Botella'),
+(12, 'BYKAHEPAR Fco x 100cc', '', 13, 'Botella'),
+(13, 'Calierxicam', '', 6, 'Botella'),
+(14, 'Camisas Sanitarias', '', 10, 'Dispositivo'),
+(15, 'CARAV. ALLFLEX TAMPER MAXI S/N', '', 100, 'Dispositivo'),
+(16, 'Caravanas Amarrillas Grandes S/N', '', 400, 'Dispositivo'),
+(17, 'CARAVANAS SENASA DP- Celestes', '', 300, 'Dispositivo'),
+(18, 'CARAVANAS VERDES GRANDES S/N', '', 250, 'Dispositivo'),
+(19, 'Cefaximin (mast?tis)', '', 63, 'Tubo'),
+(20, 'Ceftion Frasco x100cc', '', 1180, 'Mililitros'),
+(21, '', '', 0, '');
 
 -- --------------------------------------------------------
 
@@ -68,8 +87,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `email`, `edad`, `rol`, `password`) VALUES
-(1, 'Waldo', 'Cuevas', 'Waldo@example.com', 24, 1, '123'),
-(2, 'Ramon', 'Salazar', 'Ramon@example.com', 19, 0, '000');
+(1, 'Waldo', 'Cuevas', 'Waldo@gmail.com', 24, 1, '1234'),
+(2, 'Alejandro', 'Dalzotto', 'ale@gmail.com', 20, 0, '12345');
 
 --
 -- Índices para tablas volcadas
@@ -95,13 +114,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(254) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(254) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(254) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(254) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
